df7f7ceadbb6cc1aad93b89a0f1a86fe504be080
AndriyKuzmenko <59597557+AndriyKuzmenko@users.noreply.github.com>
2021-01-02 15:08:23 +0200

    Update CreditsActivity.java

--- app/src/main/java/com/example/sharedpreferences/CreditsActivity.java
+++ app/src/main/java/com/example/sharedpreferences/CreditsActivity.java
@@ -2,7 +2,10 @@ package com.example.sharedpreferences;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class CreditsActivity extends AppCompatActivity
@@ -16,12 +19,34 @@ public class CreditsActivity extends AppCompatActivity
     }
 
     /**
-     * this method runs when the back button is pressed. it goes back to the main activity.
-     * @param view
+     * @param menu  - the menu
+     * @return      - shows the main menu
      */
 
-    public void back(View view)
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
     {
-        finish();
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        return true;
+    }
+
+    /**
+     *
+     * @param item - the item that was selected
+     * @return     After the user pressed on the credits option, this method
+     *             starts CreditsActivity
+     */
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if(item.getItemId()==R.id.menuCredits);
+        {
+            Intent si = new Intent(this, CreditsActivity.class);
+            startActivity(si);
+        }
+
+        return true;
     }
 }
